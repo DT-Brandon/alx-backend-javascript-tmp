b23c6317b167d7912b063b1051973ff3946c48b5
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-07-25 22:35:35 +0000

    task 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,29 +1,29 @@
 export default function createIteratorObject(report) {
-  let departments = Object.keys(report.allEmployees);
+  const departments = Object.keys(report.allEmployees);
   let currentDepartmentIndex = 0;
   let currentEmployeeIndex = 0;
 
   return {
-    next: function() {
+    next() {
       if (currentEmployeeIndex >= report.allEmployees[departments[currentDepartmentIndex]].length) {
         currentEmployeeIndex = 0;
-        currentDepartmentIndex++;
+        currentDepartmentIndex += 1;
       }
 
       if (currentDepartmentIndex >= departments.length) {
         return {
-          done: true
+          done: true,
         };
       }
 
-      let department = departments[currentDepartmentIndex];
-      let employee = report.allEmployees[department][currentEmployeeIndex];
-      currentEmployeeIndex++;
+      const department = departments[currentDepartmentIndex];
+      const employee = report.allEmployees[department][currentEmployeeIndex];
+      currentEmployeeIndex += 1;
 
       return {
         value: employee,
-        done: false
+        done: false,
       };
-    }
+    },
   };
 }
